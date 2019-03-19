1f5bc84c16c8c13ded4d1b92e668cf0f9bf77482
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-19 15:57:36 -0300

    Formulario com Script validarForm

--- form.php
+++ form.php
@@ -43,38 +43,35 @@
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">ADBA</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Discipulado
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Cadastro de Pessoa</a>
+                        <a class="dropdown-item" href="#">Lista de chamada</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Controle</a>
+                    </div>
+                </li>
+
+            </ul>
+
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-   
-  </div>
-</nav>
+    </nav>
     <div class="card">
         <div class="card-header">
             Formulario cadastro para Discipulado ADBA
@@ -91,7 +88,7 @@
                     </div>
                 </div>
                 <div class="form-row">
-                    <div class="col-md-6 mb-3">
+                    <div class="col-md-4 mb-3">
                         <label for="telefone">Telefone</label>
                         <input type="text" class="form-control" id="telefone"
                             placeholder="(xx)x xxxx-xxxx/(xx)xxxx-xxxx" required>
@@ -101,7 +98,7 @@
                     </div>
                 </div>
                 <div class="form-row">
-                    <label class="my-1 mr-2" for="congregação">Selecione a Congregação</label>
+                    <label class="col-md-4 mb-3" for="congregação">Selecione a Congregação</label>
                     <select class="custom-select my-1 mr-sm-2" id="congregação">
                         <option selected>Choose...</option>
                         <option value="Sede">Sede</option>
@@ -110,22 +107,23 @@
                         <option value="Jd. Portugal">Jd. Portugal</option>
                     </select>
                 </div>
-        </div>
 
-        <div class="form-group">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="invalidCheck" required>
-                <label class="form-check-label" for="invalidCheck">
-                    Agree to terms and conditions
-                </label>
-                <div class="invalid-feedback">
-                    You must agree before submitting.
+
+                <div class="form-group">
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="invalidCheck" required>
+                        <label class="form-check-label" for="invalidCheck">
+                            Agree to terms and conditions
+                        </label>
+                        <div class="invalid-feedback">
+                            You must agree before submitting.
+                        </div>
+                    </div>
                 </div>
-            </div>
+                <button class="btn btn-primary" type="submit">Submit form</button>
+            </form>
         </div>
-        <button class="btn btn-primary" type="submit">Submit form</button>
-        </form>
-    </div>
     </div>
 
 
