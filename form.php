181818b9ace03b42474ffeba27c25417c833159f
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-19 17:44:16 -0300

    Formulario com Script validarForm

--- form.php
+++ form.php
@@ -11,6 +11,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script type="text/javascript" src="jQuery-Mask-Plugin/dist/jquery.mask.min.js"></script>
     <meta charset="UTF-8">
