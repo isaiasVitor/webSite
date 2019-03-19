8c18a0528eb9f5f5c8e909ab4ab07bd78028a6e8
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-19 16:20:33 -0300

    Formulario com Script validarForm

--- form.php
+++ form.php
@@ -37,10 +37,20 @@
             });
         }, false);
     })();
+    </script>
 
-    $(document).ready(function() {
-        $('#telefone').mask('(00) 0000-0000#');
-    });
+    <script>
+    import 'jQuery-Mask-Plugin';
+    var SPMaskBehavior = function(val) {
+            return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+        },
+        spOptions = {
+            onKeyPress: function(val, e, field, options) {
+                field.mask(SPMaskBehavior.apply({}, arguments), options);
+            }
+        };
+
+    $('.telefone').mask(SPMaskBehavior, spOptions);
     </script>
 
 </head>
