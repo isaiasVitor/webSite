f50a067ca98d44030dc20ff93f22c90fb966851f
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-19 16:21:24 -0300

    Formulario com Script validarForm

--- form.php
+++ form.php
@@ -40,7 +40,7 @@
     </script>
 
     <script>
-    import 'jQuery-Mask-Plugin';
+    import jQuery-Mask-Plugin;
     var SPMaskBehavior = function(val) {
             return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
         },
