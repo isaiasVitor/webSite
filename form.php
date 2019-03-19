eab5e29870e55172f06bdfff4a4b1f668d09b61d
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-19 13:37:21 -0300

    formulario

---
+++ form.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EXEMPLO - Formulário</title>
+</head>
+
+<body>
+    <form action="?action=save" namee="meuForm" method="post">
+        Nome: <input type="text" name="CAMPO_NOME">
+        <br>idade: <input type="text" name="CAMPO_IDADE">
+        <br>E-mail: <input type="email" name="CAMPO_IDADE">
+        <br>Sexo: <input type=radio name="CAMPO_SEXO" value="M">Masculino
+        <input type=radio name="CAMPO_SEXO" value="F">Feminino
+
+        <br>Curso: <select name="CAMPO_CURSO">
+            <option selected value="Selecione..."></option>
+            <option value="Ciência da Computação"></option>
+            <option value="Bacharelado em Informática"></option>
+            <option value="Engenharia da COmputação"></option>
+        </select>
+
+        <br>Conhecimentos:
+        <input type="CHECKBOX" nome="CAMPO_CONHECIMENTOS[]" value="Word">Microsoft Word
+        <input type="CHECKBOX" nome="CAMPO_CONHECIMENTOS[]" value="HTML">HTML
+        <input type="CHECKBOX" nome="CAMPO_CONHECIMENTOS[]" value="JS">JavaScript
+        <input type="CHECKBOX" nome="CAMPO_CONHECIMENTOS[]" value="PHP">PHP
+
+        <br>
+        <input type="reset" value="Limpar">
+        <input type="submit" value="Salvar">
+    </form>
+</body>
+
+</html>
