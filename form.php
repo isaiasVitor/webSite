6213010a1e97c4d8619a0fae5323e7079e7c402f
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-19 16:37:49 -0300

    Formulario com Script validarForm

--- form.php
+++ form.php
@@ -11,6 +11,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
+    
+    <script type="text/javascript" src="jquery.mask.min.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -38,7 +40,7 @@
         }, false);
     })();
     </script>
-
+    <script type="text/javascript">$("#telefone").mask("(00) 0000-00009");</script>
 </head>
 
 <body>
