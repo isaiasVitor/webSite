15bd7d86f30d407aa06c1633df0fc1e73d2444e2
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-19 17:37:19 -0300

    Formulario com Script validarForm

--- form.php
+++ form.php
@@ -39,17 +39,21 @@
             });
         }, false);
     })();
-    $("#telefone").change(function() {
-        jQuery("input.telefone").mask("(99) 9999-9999");
-        if($("#telefone").val.length>14){
-            jQuery("input.telefone").mask("(99) #9999-9999");
-        }else{
-            jQuery("input.telefone").mask("(99) 9999-9999");
-        };
 
-       
-    });
-    
+    jQuery("input.telefone")
+        .mask("(99) 9999-9999?9")
+        .focusout(function (event) {  
+            var target, phone, element;  
+            target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
+            phone = target.value.replace(/\D/g, '');
+            element = $(target);  
+            element.unmask();  
+            if(phone.length > 10) {  
+                element.mask("(99) 99999-999?9");  
+            } else {  
+                element.mask("(99) 9999-9999?9");  
+            }  
+        });
     </script>
 
 
