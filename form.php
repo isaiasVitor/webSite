48135676e7ed54253e4b95fa4d325b4578701610
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-19 16:47:54 -0300

    Formulario com Script validarForm

--- form.php
+++ form.php
@@ -44,14 +44,8 @@
     <script type="text/javascript">
     var SPMaskBehavior = function(val) {
             return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
-        },
-        spOptions = {
-            onKeyPress: function(val, e, field, options) {
-                field.mask(SPMaskBehavior.apply({}, arguments), options);
-            }
-        };
-
-    $('#telefone').mask(SPMaskBehavior, spOptions);
+        }
+    $('#telefone').mask(SPMaskBehavior);
     </script>
 
 </head>
