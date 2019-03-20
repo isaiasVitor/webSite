eb1c65c673587b501122d8b3a933957cd37880af
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-20 17:37:58 -0300

    ok

--- gerencia-registro.php
+++ gerencia-registro.php
@@ -63,7 +63,7 @@
 <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-sm">
     <div class="modal-content">
-      <? echo $conn->insert_id ?>
+      <? echo $conn->insert_id; ?>
     </div>
   </div>
 </div>
