043626fb24d4a910eab6c9e80343356597db4008
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-20 17:12:36 -0300

    ok

--- form.php
+++ form.php
@@ -1,9 +1,25 @@
+<?php
+ ini_set('default_charset','ISO-8859-1');
+ mb_internal_encoding("UTF-8"); 
+ mb_http_output( "iso-8859-1" );  
+ ob_start("mb_output_handler");   
+ header("Content-Type: text/html; charset=ISO-8859-1",true);
+?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/form.css">
+    <title>Cadastro de Aluno</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
@@ -14,13 +30,6 @@
     </script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script type="text/javascript" src="jQuery-Mask-Plugin/dist/jquery.mask.min.js"></script>
-    <meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <link rel="stylesheet" type="text/css" href="css/form.css">
-    <title>Cadastro de Aluno</title>
-   
     <script>
     // Example starter JavaScript for disabling form submissions if there are invalid fields
     (function() {
