3b5809c8c28e43a742eefb715f9f9af54e6a8570
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-20 15:29:19 -0300

    ok

--- gerencia-registro.php
+++ gerencia-registro.php
@@ -1,20 +1,19 @@
 <?PHP
     
-    #dados para a conexão com o banco de dados
+    #dados para a conexão com o banco de dados;
     $servidor = 'localhost'; #ip do servidor
     $usuario = 'isaias';          #Usuario para acessar o banco
     $senha = 'kyzasd39XZ#@';      #Senha do banco
     $banco = 'adbadb';            #nome do database
 
-    #executa a conexão com o mysql
+    #executa a conexão com o mysql;
     
     $conn = new mysqli($servidor, $usuario, $senha, $banco);
     if ($conn->connect_error) {
         die("conexão falhou: " . $conn->connect_error);
     } 
-    echo json_encode($_REQUEST);
-    /*
-    #verifica se o arquivo foi chamado a partir de um formulário
+   
+    #verifica se o arquivo foi chamado a partir de um formulário;
     if($_REQUEST["acao"] == "adicionar"){
         #FAZENDO O INSERT
         $sql = "INSERT INTO alunos (nome, telefone_fixo, telefone_celular, congregacao) VALUES (";
@@ -24,16 +23,13 @@
         $sql .= "'".$_REQUEST["congregacao"]."', ";
         $sql .= ");";
 
-        #executa a expressão SQL no servidor e armazena o resultado
-        $result = mysql_query($sql);
-
-        if ($conn->query($sql) === TRUE) {
+         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
-    }  
-    */
+    }
+    
         $conn->close();
   
     
