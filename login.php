e2f0c08eb542d5cf641f1074fda184c576823300
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-20 00:04:41 -0300

    Form completo

--- login.php
+++ login.php
@@ -25,7 +25,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <link rel="stylesheet" type="text/css" href="css/form.css">
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
 </head>
 
 <body class="text-center">
