8bda7b050a6cfc4d3e51ff483b67f1c32f310d2c
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-20 17:39:40 -0300

    ok

--- gerencia-registro.php
+++ gerencia-registro.php
@@ -63,14 +63,14 @@
 <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-sm">
     <div class="modal-content">
-      <? echo $conn->insert_id; ?>
+      <?PHP echo $conn->insert_id; ?>
     </div>
   </div>
 </div>
 </body>
 
 </html>
-<?               
+<?PHP              
                 //header('Location: form.php');
                
             } else {
