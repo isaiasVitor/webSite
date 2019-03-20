039abe05c227c558f9022f2fecaca9d22f0d6d3c
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-20 10:20:20 -0300

    sem comentarios

--- table.php
+++ table.php
@@ -43,21 +43,21 @@
                     <tbody>
                         <tr>
                             <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
+                            <td>Isaias Silva</td>
+                            <td>(16) 99750-5196</td>
+                            <td>Sede</td>
                         </tr>
                         <tr>
                             <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
+                            <td>Marcos Silva</td>
+                            <td>(16) 99750-5196</td>
+                            <td>Sede</td>
                         </tr>
                         <tr>
                             <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
+                            <td>Maria Joaquina Texeira</td>
+                            <td>(16)3333-2839</td>
+                            <td>Jd. Portugal</td>
                         </tr>
                     </tbody>
                 </table>
