81b9040b449f66a66352a5803e89ab3b238cfaa9
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-20 14:48:36 -0300

    ok

--- gerencia-registro.php
+++ gerencia-registro.php
@@ -1,5 +1,5 @@
 <?PHP
-
+    
     #dados para a conexão com o banco de dados
     $servidor = '18.228.137.119'; #ip do servidor
     $usuario = 'isaias';          #Usuario para acessar o banco
@@ -7,33 +7,33 @@
     $banco = 'adbadb';            #nome do database
 
     #executa a conexão com o mysql
-    $link = mysql_connect($servidor, $usuario, $senha) or die('Não foi possivel conectar: '.mysql_error());
-
-    #seleciona o banco de dados que deseja utilizar
-    $select = mysql_select_db($banco);
+    
+    $conn = new mysqli($servidor, $usuario, $senha, $banco);
+    if ($conn->connect_error) {
+        die("conexão falhou: " . $conn->connect_error);
+    } 
 
     #verifica se o arquivo foi chamado a partir de um formulário
     if($_REQUEST["acao"] == "adicionar"){
         #FAZENDO O INSERT
-        $sql = "INSERT INTO ALUNOS (NOME,TELEFONE_FIXO,TELEFONE_CELULAR,CONGREGACAO) VALUES (";
+        $sql = "INSERT INTO alunos (nome, telefone_fixo, telefone_celular, congregacao) VALUES (";
         $sql .= "'".$_REQUEST["nome"]."', ";
         $sql .= "'".$_REQUEST["telefone_fixo"]."', ";
         $sql .= "'".$_REQUEST["telefone_celular"]."', ";
         $sql .= "'".$_REQUEST["congregacao"]."', ";
-        $sql .= ")";
+        $sql .= ");";
 
         #executa a expressão SQL no servidor e armazena o resultado
         $result = mysql_query($sql);
 
-        #verifica o sucesso da operação
-        if(!result)
-        {
-            die('Erro:' .mysql_error());
-        }else{
-            #se a operação foi realizada com sucesso, informa na tela
-            echo 'A Operação foi realizada com sucesso.';
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
         }
+        
+        $conn->close();
   
     }
-    echo 'oi'
+
 ?>
