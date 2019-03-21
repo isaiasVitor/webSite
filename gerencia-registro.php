65d1de544d0ab96aab06b7e7bc19b10bd9386e80
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 00:41:42 -0300

    ok

--- gerencia-registro.php
+++ gerencia-registro.php
@@ -60,9 +60,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script>
     $(document).ready(function() {
-        $("button").click(function() {
-            $("p").hide();
-        });
+        $('#exemplomodal').modal('show');
     });
     </script>
 
