053f571083a11da8bce4eee7f29390312261b1ef
Isaias Silva <isaiasvitordealmeidasilva@gmail.com>
2019-03-21 08:07:59 -0300

    ok

--- gerencia-registro.php
+++ gerencia-registro.php
@@ -57,7 +57,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/jquery-3.3.1.min.js"></script>
     <script>
     $(document).ready(function() {
         $('#exampleModalCenter').modal('show');
